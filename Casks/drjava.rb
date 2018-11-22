b0d0fc80c4e002d8f88f8757eec207b2aa72de10
Hansel Wei <hwei3@uncc.edu>
2018-11-22 00:52:50 -0500

    test 2

--- Casks/drjava.rb
+++ Casks/drjava.rb
@@ -3,7 +3,7 @@ cask 'drjava' do
   sha256 '701c3638049b99aa59cfcb2a9874493496d20ffb34370e67935bb6f64fa5ae91'
 
   # downloads.sourceforge.net/drjava/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/projects/drjava/files/1.%20DrJava%20Stable%20Releases/drjava-#{version.after_comma}-#{version.before_comma}/drjava-#{version.after_comma}-#{version.before_comma}.jar"
+  url "https://sourceforge.net/projects/drjava/files/1.%20DrJava%20Stable%20Releases/drjava-#{version.after_comma}-#{version.before_comma}/drjava-#{version.after_comma}-#{version.before_comma}.jar/download"
   appcast 'https://sourceforge.net/projects/drjava/rss?path=/1.%20DrJava%20Stable%20Releases'
   name 'DrJava'
   homepage 'http://www.drjava.org/'
