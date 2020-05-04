02b812c4789c1f5b9ad3a19dec2c62f3832f2a4c
Hansel Wei <hello@hanselwei.dev>
2020-05-04 02:35:33 -0400

    Update drjava.rb

--- Casks/drjava.rb
+++ Casks/drjava.rb
@@ -1,15 +1,31 @@
 cask 'drjava' do
-  version '20160913-225446,beta'
-  sha256 'fd4e24ac4901accbdc37644149ecbf54644b4a073b1963d344dd49520a44b430'
+  version '20190813-220051,beta'
+  sha256 '69bdccf632ca8396876bd61cd362027d9ec90ea0a4e8b4edadec438d5b253ff9'
 
   # downloads.sourceforge.net/drjava/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/projects/drjava/1.%20DrJava%20Stable%20Releases/drjava-#{version.after_comma}-#{version.before_comma}/drjava-#{version.after_comma}-#{version.before_comma}.jar"
+  url "https://downloads.sourceforge.net/drjava/drjava-#{version.after_comma}-#{version.before_comma}.jar"
   appcast 'https://sourceforge.net/projects/drjava/rss?path=/1.%20DrJava%20Stable%20Releases'
   name 'DrJava'
   homepage 'http://www.drjava.org/'
 
-  depends_on java: '1.8'
   container type: :naked
 
-  app "drjava-#{version.after_comma}-#{version.before_comma}/#{version.after_comma}-#{version.before_comma}.jar", target: 'DrJava.jar'
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/drjava.wrapper.sh"
+  binary shimscript, target: 'drjava'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      cd "$(dirname "$(readlink -n "$0")")" && \
+        nohup java "$@" -jar drjava-#{version.after_comma}-#{version.before_comma}.jar \
+        >/dev/null 2>&1 &
+    EOS
+  end
+
+  zap trash: '~/.drjava'
+
+  caveats do
+    depends_on_java '8'
+  end
 end
